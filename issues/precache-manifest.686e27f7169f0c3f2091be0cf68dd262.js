dcae74776479bb8ceceacef11d66b04a49a3f366
Bob Webb <bob.wbb@gmail.com>
2019-11-05 19:43:39 +0000

    add issues browser

---
+++ issues/precache-manifest.686e27f7169f0c3f2091be0cf68dd262.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fb02862b5edce551997fd52a469ce2",
+    "url": "/issues/index.html"
+  },
+  {
+    "revision": "f2d189e7fb70f24876a5",
+    "url": "/issues/static/css/main.6387b0a8.chunk.css"
+  },
+  {
+    "revision": "b3ae381064b68aeb0306",
+    "url": "/issues/static/js/2.84e62073.chunk.js"
+  },
+  {
+    "revision": "f2d189e7fb70f24876a5",
+    "url": "/issues/static/js/main.f8fdfe12.chunk.js"
+  },
+  {
+    "revision": "a3a30e9b76aaf0f891cf",
+    "url": "/issues/static/js/runtime-main.ed6e067c.js"
+  }
+]);
